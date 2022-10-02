2fd4ddf29f2be0bf8f0a8cd921b42b1d6a68e0e3
Rafael <rafaelproinfo@gmail.com>
2022-10-02 15:50:16 -0300

    Update

---
+++ src/conta/controller/ContaController.java
@@ -0,0 +1,56 @@
+package conta.controller;
+
+import conta.model.Conta;
+import conta.repository.ContaRepository;
+
+public class ContaController implements ContaRepository {
+
+    @Override
+    public void procurarPorNumero(int numero) {
+        
+        
+    }
+
+    @Override
+    public void listarTodas() {
+        
+        
+    }
+
+    @Override
+    public void cadastrar(Conta conta) {
+        
+        
+    }
+
+    @Override
+    public void atualizar(Conta conta) {
+        
+        
+    }
+
+    @Override
+    public void deletar(int numero) {
+        
+        
+    }
+
+    @Override
+    public void sacar(int numero, float valor) {
+        
+        
+    }
+
+    @Override
+    public void depositar(int numero, float valor) {
+        
+        
+    }
+
+    @Override
+    public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+        
+        
+    }
+    
+}
