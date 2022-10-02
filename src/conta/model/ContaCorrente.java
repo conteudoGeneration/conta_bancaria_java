a94590c4f66efa650cefa18bba575b266546e931
Rafael <rafaelproinfo@gmail.com>
2022-10-02 15:35:08 -0300

    Update

---
+++ src/conta/model/ContaCorrente.java
@@ -0,0 +1,38 @@
+package conta.model;
+
+public class ContaCorrente extends Conta{
+
+	private float limite;
+
+	public ContaCorrente(int numero, int agencia, int tipo, String titular, float saldo, float limite) {
+		super(numero, agencia, tipo, titular, saldo);
+		this.limite = limite;
+	}
+	
+	public float getLimite() {
+		return limite;
+	}
+
+	public void setLimite(float limite) {
+		this.limite = limite;
+	}
+
+	@Override
+	public boolean sacar(float valor) { 
+		
+		if(this.getSaldo() + this.getLimite() < valor) {
+			System.out.println("\n Saldo Insuficiente!");
+			return false;
+		}
+		
+		this.setSaldo(this.getSaldo() - valor);
+		return true;
+		
+	}
+	
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("Limite de Crédito: " + this.limite);
+	}
+    
+}
---
+++ src/conta/model/ContaPoupanca.java
@@ -0,0 +1,25 @@
+package conta.model;
+
+public class ContaPoupanca extends Conta{
+
+	private int aniversario;
+
+	public ContaPoupanca(int numero, int agencia, int tipo, String titular, float saldo, int aniversario) {
+		super(numero, agencia, tipo, titular, saldo);
+		this.aniversario = aniversario;
+	}
+
+	public int getAniversario() {
+		return aniversario;
+	}
+
+	public void setAniversario(int aniversario) {
+		this.aniversario = aniversario;
+	}
+	
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("Aniversário da conta: " + this.aniversario);
+	}
+    
+}
