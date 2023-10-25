3072f6a7dce36b03f2172b57a824d34183ab3d89
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-10-25 14:32:34 -0300

    Update

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 import conta.util.Cores;
 
 public class Menu {
-    public static void main(String[] args) {
-        
-        Scanner leia = new Scanner(System.in);
-		
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
 		int opcao;
-				
-		while(true) {
+
+		while (true) {
 
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
 					+ "*****************************************************");
@@ -33,53 +33,63 @@ public class Menu {
 			System.out.println("*****************************************************");
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     " + Cores.TEXT_RESET);
-			
+
 			opcao = leia.nextInt();
-				
+
 			if (opcao == 9) {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				sobre();
 				leia.close();
 				System.exit(0);
 			}
-				
+
 			switch (opcao) {
 				case 1:
 					System.out.println(Cores.TEXT_WHITE + "Criar Conta Corrente\n\n");
-				
-                    break;
+
+					break;
 				case 2:
 					System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
-					
-                    break;
+
+					break;
 				case 3:
 					System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
-	
-                    break;
+
+					break;
 				case 4:
 					System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
-					
-                    break;
+
+					break;
 				case 5:
 					System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
-		
-                    break;
+
+					break;
 				case 6:
 					System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
 
-                    break;
+					break;
 				case 7:
 					System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
-					
-                    break;
+
+					break;
 				case 8:
 					System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
-					
-                    break;
+
+					break;
 				default:
 					System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
-                    break;
+					break;
 			}
-		}	
-    }
+		}
+
+	}
+
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("Generation Brasil - generation@generation.org");
+		System.out.println("github.com/conteudoGeneration");
+		System.out.println("*********************************************************");
+	}
 
 }
