cac48436f1738fe4d3b57c90313c7a0fb8fa8483
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-10-25 14:39:15 -0300

    Update

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -8,12 +8,12 @@ import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
-    public static void main(String[] args) {
-        
-        Scanner leia = new Scanner(System.in);
-		
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
 		int opcao;
-		
+
 		// Teste da Classe Conta
 		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
 		c1.visualizar();
@@ -21,7 +21,7 @@ public class Menu {
 		c1.visualizar();
 		c1.depositar(5000.0f);
 		c1.visualizar();
-		
+
 		// Teste da Classe Conta Corrente
 		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
 		cc1.visualizar();
@@ -37,8 +37,8 @@ public class Menu {
 		cp1.visualizar();
 		cp1.depositar(5000.0f);
 		cp1.visualizar();
-		
-		while(true) {
+
+		while (true) {
 
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
 					+ "*****************************************************");
@@ -60,53 +60,61 @@ public class Menu {
 			System.out.println("*****************************************************");
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     " + Cores.TEXT_RESET);
-			
+
 			opcao = leia.nextInt();
-				
+
 			if (opcao == 9) {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				sobre();
 				leia.close();
 				System.exit(0);
 			}
-				
+
 			switch (opcao) {
 				case 1:
 					System.out.println(Cores.TEXT_WHITE + "Criar Conta Corrente\n\n");
-				
-                    break;
+
+					break;
 				case 2:
 					System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
-					
-                    break;
+
+					break;
 				case 3:
 					System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
-	
-                    break;
+
+					break;
 				case 4:
 					System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
-					
-                    break;
+
+					break;
 				case 5:
 					System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
-		
-                    break;
+
+					break;
 				case 6:
 					System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
 
-                    break;
+					break;
 				case 7:
 					System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
-					
-                    break;
+
+					break;
 				case 8:
 					System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
-					
-                    break;
+
+					break;
 				default:
 					System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
-                    break;
+					break;
 			}
-		}	
-    }
+		}
+	}
 
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("Generation Brasil - generation@generation.org");
+		System.out.println("github.com/conteudoGeneration");
+		System.out.println("*********************************************************");
+	}
 }
