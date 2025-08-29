7528925ee5bed4ff159fe75211d49c3093c4b3da
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-08-29 16:43:06 -0300

    Update Menu

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -25,7 +25,7 @@ public class Menu {
 			System.out.println("            6 - Sacar                                ");
 			System.out.println("            7 - Depositar                            ");
 			System.out.println("            8 - Transferir valores entre Contas      ");
-			System.out.println("            9 - Sair                                 ");
+			System.out.println("            0 - Sair                                 ");
 			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
 			System.out.println("Entre com a opção desejada:                          ");
@@ -33,7 +33,7 @@ public class Menu {
 
 			opcao = leia.nextInt();
 
-			if (opcao == 9) {
+			if (opcao == 0) {
 				System.out.println("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
 				sobre();
 				leia.close();
